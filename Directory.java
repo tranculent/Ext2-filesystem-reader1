5576f81d6697af5a25925698b8977b16447a0255
Mirov <mirov@egb080000033.lancs.local>
2019-12-05 22:35:04 +0000

    Need to fix lost+found and big-dir directories

--- Directory.java
+++ Directory.java
@@ -37,53 +37,74 @@ public class Directory {
             System.out.println(inodes[fileInfoArray[i].getInodeNum() - 1] + " " + fileInfoArray[i].getName());
 
        /* TRAVERSE Directories enlisted */
+
+       // FIX lost+found and big-dir - not returning from there, unable to call cd ..l
        while (true) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(">>>");
-        String answer = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            System.out.print(">>>");
+            String answer = scanner.nextLine();
 
-        if (answer.equals("ls")) 
-            for (int i = 0; i < fileInfoArray.length; i++)
-                System.out.println(inodes[fileInfoArray[i].getInodeNum() - 1] + " " + fileInfoArray[i].getName());
-        
-        if (answer.equals("exit")) break;
-        
-        for (int i = 0; i < fileInfoArray.length; i++) {
-            if (answer.equals("cd " + fileInfoArray[i].getName())) {
-                Inode inodeToBeSearched = inodes[fileInfoArray[i].getInodeNum() - 1];
-                for (int j = 0; j < inodeToBeSearched.getDataBlockPointers().length; j++) {
-                    if (inodeToBeSearched.getDataBlockPointers()[j] != 0) {
-                        Inode contentToBeRead = new Inode(inodeToBeSearched.getDataBlockPointers()[j] * 1024, file);
+            if (answer.equals("ls")) {
 
-                        fileInfos.clear();
+                System.out.println("Array in ls: " + fileInfoArray.length);
+                for (int i = 0; i < fileInfoArray.length; i++) {
+                    if (fileInfoArray[i].getInodeNum() > 0)
+                        System.out.println(inodes[fileInfoArray[i].getInodeNum() - 1] + " " + fileInfoArray[i].getName());
+                    else 
+                        System.out.println(inodes[fileInfoArray[i].getInodeNum()] + " " + fileInfoArray[i].getName());
+                }
+            }   
+            
+            if (answer.equals("exit")) break;
+int index =0;
+            for (int i = 0; i < fileInfoArray.length; i++) {
 
-                        if (!contentToBeRead.isFile()) {
+                if (answer.equals("cd " + fileInfoArray[i].getName())) {
+                    Inode inodeToBeSearched = inodes[fileInfoArray[i].getInodeNum() > 0 ? fileInfoArray[i].getInodeNum() - 1 : fileInfoArray[i].getInodeNum()];
+
+                    for (int j = 0; j < inodeToBeSearched.getDataBlockPointers().length; j++) {
+
+                        if (inodeToBeSearched.getDataBlockPointers()[j] != 0) {
+
+                            fileInfos.clear();
+
                             offset = 0;
+
                             while (offset < 1024) {
+
                                 FileInfo fInfo = new FileInfo(offset + inodeToBeSearched.getDataBlockPointers()[j] * 1024, file);
                                 fileInfos.add(fInfo);
+                                System.out.println("File Info: " + index++);
                                 offset += fInfo.getLength();
-                                // System.out.println("Inode: " + inodes[fInfo.getInodeNum() - 1] + " " + fInfo.getName() + ", length: " + fInfo.getLength());
-                            }
-                    
-                            // FileInfo[] innerFilesArray = new FileInfo[fileInfos.size()];
+                                // System.out.println("File Info: " + fileInfos.size());
+                            }                        
+                            
                             fileInfoArray = new FileInfo[fileInfos.size()];
                             fileInfoArray = fileInfos.toArray(fileInfoArray);
                     
-                            // FIX
                             for (int k = 0; k < fileInfoArray.length; k++) {
                                 if (fileInfoArray[k].getInodeNum() < 5136) {
-                                    System.out.println(inodes[fileInfoArray[k].getInodeNum() - 1] + " " + fileInfoArray[k].getName());
+                                    if (fileInfoArray[k].getInodeNum() > 0)
+                                        System.out.println(inodes[fileInfoArray[k].getInodeNum() - 1] + " " + fileInfoArray[k].getName());
+                                    else 
+                                        System.out.println(inodes[fileInfoArray[k].getInodeNum()] + " " + fileInfoArray[k].getName());
                                 }
                             }
+                            
                         }
-                        else
-                            System.out.println("nope:");
                     }
+                }
+                else if (answer.equals("cat " + fileInfoArray[i].getName())) {
+                    Inode inodeToBeSearched = inodes[fileInfoArray[i].getInodeNum() > 0 ? fileInfoArray[i].getInodeNum() - 1 : fileInfoArray[i].getInodeNum()];
+                    for (int j = 0; j < inodeToBeSearched.getDataBlockPointers().length; j++) {
+                        if (inodeToBeSearched.getDataBlockPointers()[j] != 0) {
+                            System.out.println(new String(file.read(inodeToBeSearched.getDataBlockPointers()[j] * 1024, 1024)));
+                        }
+                    }
+                    
                 }
             }
         }
-    }
     }
 
     public FileInfo[] getFileInfo() {
